039ba89324e019c570a9e84c9fbd814df9fa81f7
Joaquin de la Sierra <joaquin@joaquin.cc>
2013-05-21 17:34:09 -0500

    Added the main getSkeleton

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -69,8 +69,15 @@ class Main extends CI_Controller
     public function index()
     {
         
-        //
+        $this->load->view('main');
         
+    }
+    
+    public function signup()
+    {
+	    
+	    
+	    
     }
     
 }
