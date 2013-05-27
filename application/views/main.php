18b9d5700f97571ed1941801c793a4e9515091bf
Joaquin de la Sierra <joaquin@joaquin.cc>
2013-05-27 17:46:09 -0500

    Added Right Like Dislike

--- application/views/main.php
+++ application/views/main.php
@@ -42,16 +42,28 @@
 	================================================== -->
 
 	<!-- Delete everything in this .container and get started on your own site! -->
-
-	<div id="nav"><p class="title"><a href="./">PositiveThoughts</a></p><ul id="navigation">  <li><a href="./">Home</a></li>   <li><a href="top">Top</a></li>     <li><a href="submit">Submit</a></li><?php if (!$this->tank_auth->is_logged_in()) { ?><li><a href = 'login'>login</a><li><a href = 'signup'>signup</a></li><?php } ?></ul></div>
-
+	
 	<div class="container">
-		<div class="sixteen columns">
-			<h1 class="remove-bottom" style="margin-top: 40px"><?php echo $title; ?></h1><?=$username?>
-			<hr />
+
+	
+	<h1 class="remove-bottom"><?php echo $title; ?></h1><ul id="navigation">  <li><a href="./">Home</a></li>   <li><a href="top">Top</a></li>     <li><a href="submit">Submit</a></li><?php if (!$this->tank_auth->is_logged_in()) { ?><li><a href = 'login'>login</a><li><a href = 'signup'>signup</a></li><?php } ?></ul>
+	
+		
+	<hr />
+	
+	
+		
+		<div class="twelve columns">
 			<p><?php echo $content; ?></p>
-			
 		</div>
+
+	
+		<div class="four columns">
+			
+			<p><?php echo $contentTitle; ?></p>
+
+		</div>
+	
 
 	</div><!-- container -->
 
