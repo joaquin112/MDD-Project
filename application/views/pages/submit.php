e479aae36ff11ec2b938c8ccc0cb942f5761ce91
Joaquin de la Sierra <joaquin@joaquin.cc>
2013-05-29 12:21:35 -0500

    added meta tags functionality

--- application/views/pages/submit.php
+++ application/views/pages/submit.php
@@ -1,8 +1,9 @@
 <?php if ($username != "") { ?>
 
 <form id="form_id" name="form_name" action="forms/submit" method="post">
-	<p><label for="first">Enter image title</label><input type="text" name="title" id="title" /></p>
-	<p><label for="first">Enter image URL</label><input type="text" name="imageurl" id="imageurl" /></p>
+	<p><label>Enter image title</label><input type="text" name="title" id="title" /></p>
+	<p><label>Enter image URL</label><input type="text" name="imageurl" id="imageurl" /></p>
+	<p><label>Enter meta tags separated by comas</label><textarea style = "width: 100%; height: 10px;" name = "meta_tags"></textarea></p>
 	<input type="submit" name="submit" value="submit" />
 </form>
 
